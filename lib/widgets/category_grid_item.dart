da1bc46d9a490aa397e0641f743f25a257d03817
Ahmad Nidal Delea <a7mad.delea@gmail.com>
2024-10-21 08:32:07 +0300

    Category Grid Item. Make the widget compressible.

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -9,24 +9,29 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(22),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.54),
-            category.color.withOpacity(0.9),
-          ],
-          begin: Alignment.bottomRight,
-          end: Alignment.topLeft,
+    return InkWell(
+      onTap: () {},
+      borderRadius: BorderRadius.circular(22),
+      splashColor: Theme.of(context).colorScheme.primary,
+      child: Container(
+        padding: const EdgeInsets.all(22),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.54),
+              category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.bottomRight,
+            end: Alignment.topLeft,
+          ),
         ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-              color: Theme.of(context).colorScheme.onSurface,
-            ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+        ),
       ),
     );
   }
