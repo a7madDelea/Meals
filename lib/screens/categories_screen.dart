444d1a6d94bee2bc4ce5cddd8b3a090772f2e838
Ahmad Nidal Delea <a7mad.delea@gmail.com>
2024-10-21 07:57:25 +0300

    Categories Screen. Create CategoriesGridView.

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/categories_data.dart';
+
+import '../models/category_model.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Pick your category'),
       ),
-      body: const Center(
-        child: Text('Categories Screen'),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+          childAspectRatio: 3 / 2,
+        ),
+        children: [
+          for (CategoryModel category in categoriesData) Text(category.title),
+        ],
       ),
     );
   }
